299d90c0d6a7cfeb18e8cf97648d310cd3b0a8e8
liziqiao <1240148677@qq.com>
2017-12-13 00:49:32 +0800

    Create server.go

---
+++ service/service/server.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+)
+
+
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{IndentJSON: true})
+
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+
+	initRoutes(mx, formatter)
+
+	n.UseHandler(mx)
+	return n
+}
+
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+
+	//Get user key
+	mx.HandleFunc("/key/getkey", getUserKeyHandler(formatter)).Methods("GET")
+
+	//Create a new user
+	mx.HandleFunc("/key/register", createNewUserHandler(formatter)).Methods("POST")
+
+	//List all Users
+	mx.HandleFunc("/users/allusers", listAllUsersHandler(formatter)).Methods("GET")
+
+	//Delete current user
+	mx.HandleFunc("/users/deleteuser", deleteCurrentUserHandler(formatter)).Methods("DELETE")
+
+	//List all meetings
+	mx.HandleFunc("/meetings/allmeetings", listAllMeetingsHandler(formatter)).Methods("GET")
+
+	//Create a new meeting
+	mx.HandleFunc("/meetings/newmeeting", createNewMeetingHandler(formatter)).Methods("POST")
+
+	//Clear all meetings
+	mx.HandleFunc("/meetings/deletemeetings", clearMeetingsHandler(formatter)).Methods("DELETE")
+}
