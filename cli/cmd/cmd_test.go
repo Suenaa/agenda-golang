91732c5ba49a45bd541d8dee0776d6146ab26501
Suenaa <1165153863@qq.com>
2017-12-17 02:45:13 +0800

    commit cmd_test

--- cli/cmd/cmd_test.go
+++ cli/cmd/cmd_test.go
@@ -3,10 +3,13 @@ package cmd
 import (
 	"fmt"
 	"testing"
+	"github.com/Suenaa/agenda-golang/service/service"
 )
 
 func TestRegister(t *testing.T) {
 	fmt.Println("------TEST of Registering------")
+	server := service.NewServer()
+	server.Run(":8080")
 	registCmd.Flags().Set("username", "user")
 	registCmd.Flags().Set("password", "123")
 	registCmd.Flags().Set("email", "111@mail.com")
@@ -16,6 +19,8 @@ func TestRegister(t *testing.T) {
 
 func TestLogin(t *testing.T) {
 	fmt.Println("------TEST of Login------")
+	server := service.NewServer()
+	server.Run(":8080")
 	logoutCmd.Run(logoutCmd, nil)
 	loginCmd.Flags().Set("username", "user")
 	loginCmd.Flags().Set("password", "123")
@@ -24,6 +29,8 @@ func TestLogin(t *testing.T) {
 
 func TestLogout(t *testing.T) {
 	fmt.Println("------TEST of Logout------")
+	server := service.NewServer()
+	server.Run(":8080")
 	loginCmd.Flags().Set("username", "user")
 	loginCmd.Flags().Set("password", "123")
 	loginCmd.Run(loginCmd, nil)
@@ -32,11 +39,15 @@ func TestLogout(t *testing.T) {
 
 func TestListAllUsers(t *testing.T) {
 	fmt.Println("------TEST of Listing all users------")
+	server := service.NewServer()
+	server.Run(":8080")
 	lsuCmd.Run(lsuCmd, nil)
 }
 
 func TestCreateNewMeeting(t *testing.T) {
 	fmt.Println("------TEST of Creating a new meeting------")
+	server := service.NewServer()
+	server.Run(":8080")
 	cmCmd.Flags().Set("title", "testMeeting")
 	cmCmd.Flags().Set("participant", "testUser0,testUser1")
 	cmCmd.Flags().Set("start", "2017-10-20T19:00")
@@ -46,10 +57,14 @@ func TestCreateNewMeeting(t *testing.T) {
 
 func TestListAllMeetings(t *testing.T) {
 	fmt.Println("------TEST of Listing all meetings------")
+	server := service.NewServer()
+	server.Run(":8080")
 	lsmCmd.Run(lsmCmd, nil)
 }
 
 func TestClearMeetings(t *testing.T) {
 	fmt.Println("------TEST of clearing all meetings------")
+	server := service.NewServer()
+	server.Run(":8080")
 	clearCmd.Run(clearCmd, nil)
 }
